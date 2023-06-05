00c8884daa0c121ced38eb7c0eabacf9ed5494f0
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-06-05 11:16:30 -0300

    :recycle: With Tweet Model

--- twitter-api/main.py
+++ twitter-api/main.py
@@ -2,7 +2,7 @@
 from typing import Optional
 from enum import Enum
 from uuid import UUID # Universal Unique Identified
-from datetime import date
+from datetime import date, datetime
 # Pydantic
 from pydantic import BaseModel
 from pydantic import Field, HttpUrl, FilePath, DirectoryPath, EmailStr, PaymentCardNumber, IPvAnyAddress, NegativeFloat, PositiveFloat, NegativeInt, PositiveInt # Function para validar Models
@@ -18,12 +18,6 @@ from fastapi import Body, Query, Path, Form, Header, Cookie, File, UploadFile # 
 app = FastAPI()
 
 # Models:
-
-class Tweet(BaseModel):
-    tweet_id: UUID = Field(
-        ...,
-        example='bd65600d-8669-4903-8a14-af88203add38'
-    )
 
 class UserBase(BaseModel):
     user_id: UUID = Field(
@@ -55,6 +49,27 @@ class UserLogin(UserBase):
         ...,
         min_length=8
     )
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(
+        ...,
+        example='bd65600d-8669-4903-8a14-af88203add38'
+    )
+    content: str = Field(
+        ...,
+        max_length=256,
+        min_length=1
+    )
+    created_at: date = Field(
+        default=datetime.now()
+    )
+    updated_at: Optional[date] = Field(
+        default=None
+    )
+    by: User = Field(
+        ...
+    )
+
 
 # Home
 @app.get(

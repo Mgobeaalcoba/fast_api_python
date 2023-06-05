a718efd52da737e319a6b20afd0c3519b973d9f2
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-06-05 11:26:42 -0300

    :recycle: With User's path operations

--- twitter-api/main.py
+++ twitter-api/main.py
@@ -1,5 +1,5 @@
 # Python
-from typing import Optional
+from typing import Optional, List
 from enum import Enum
 from uuid import UUID # Universal Unique Identified
 from datetime import date, datetime
@@ -70,10 +70,77 @@ class Tweet(BaseModel):
         ...
     )
 
+# Path Operations
 
-# Home
+## Home
+
 @app.get(
     path="/",
     )
 def home():
     return {"Twitter API": "Working!"}
+
+## Users
+
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def signup():
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def login():
+    pass
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Shows all users",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"]
+)
+def show_a_user():
+    pass
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["Users"]
+)
+def update_a_user():
+    pass
+
+## Tweets
+
